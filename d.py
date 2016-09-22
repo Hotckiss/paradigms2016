cdbfb5aa9e3c3e8895ca9f5f7f95a4721c3910cc
Hotckiss <kirilenko_aA@mail.ru>
2016-09-22 20:00:44 +0300

    Add files via upload

---
+++ d.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from hashlib import sha1
+from collections import defaultdict
+def hashfile(name):
+    with open(name, 'rb') as f:
+            h = sha1()
+            while True:
+                data = f.read(65536)
+                if not data:
+                    break
+                h.update(data)
+    return h.hexdigest()
+def main():
+    if len(sys.argv) != 2:
+        print('usage: ./d.py file')
+        sys.exit(1)
+    dct = defaultdict(list)
+    for d, _, files in os.walk(sys.argv[1]):
+        for f in files:
+            if f[0] == '.' or f[0] == '~':
+                pass
+            else:
+                path = os.path.join(d, f)
+                dct[hashfile(path)].append(path)
+    for value in dct.values():
+        print(*value, sep = ':')    
+if __name__ == "__main__":
+    main()
