3276d126fe82fe6a746bd88293e9907e1932cc40
Hotckiss <kirilenko_aA@mail.ru>
2016-10-09 20:21:15 +0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,191 @@
+class Scope(object):
+    
+
+    def __init__(self, parent = None):
+        self.parent = parent
+        self.dictionary = dict()
+    def __setitem__(self, key, value):
+        self.dictionary[key] = value
+    def __getitem__(self, key):
+        if key not in self.dictionary:
+            if self.parent:
+                return self.parent[key]
+        return self.dictionary[key]
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+    def evaluate(self, scope):
+        return self
+        
+class Function:
+    def __init__(self, args, body):
+        self.args = args
+        self.body = body
+    def evaluate(self, scope):
+        function_value = None
+        for action in self.body:
+            function_value = action.evaluate(scope)
+        return function_value
+        
+class FunctionDefinition:
+    def __init__(self, name, function):
+        self.name = name
+        self.function = function
+
+    def evaluate(self, scope):
+        scope[self.name] = self.function
+        return self.function
+        
+class Conditional:
+    def __init__(self, condition, if_true, if_false=None):
+        self.condition = condition
+        self.if_true = if_true
+        self.if_false = if_false
+
+    def evaluate(self, scope):
+        condition_result = None
+        branch = self.if_true if self.condition.evaluate(scope).value else self.if_false
+        if branch is not None:
+            for i in branch:
+                condition_result = i.evaluate(scope)
+        return condition_result
+
+
+class Print:
+
+    def __init__(self, expr):
+        self.expr = expr
+    def evaluate(self, scope):
+        result = self.expr.evaluate(scope)
+        print(result.value)
+        return result
+
+class Read:
+
+    def __init__(self, name):
+        self.name = name
+    def evaluate(self, scope):
+        num = int(input())
+        scope[self.name] = Number(num)
+        return scope[self.name]
+
+class FunctionCall:
+    def __init__(self, fun_expr, args):
+        self.fun_expr = fun_expr
+        self.args = args
+
+    def evaluate(self, scope):
+        self.function = self.fun_expr.evaluate(scope)
+        self.call_scope = Scope(scope)
+        for name, ar in zip(self.function.args, self.args):
+            self.call_scope[name] = ar.evaluate(scope)
+        return self.function.evaluate(self.call_scope)
+
+class Reference:
+
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, scope):
+        return scope[self.name]
+
+
+class BinaryOperation:
+    def __init__(self, lhs, op, rhs):
+        self.lhs = lhs
+        self.op = op
+        self.rhs = rhs
+    dct = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+    '%': lambda x, y: x % y,
+    '&&': lambda x, y: 1 if x and y else 0,
+    '||': lambda x, y: 1 if x or y else 0,
+    '==': lambda x, y: 1 if x == y else 0,
+    '!=': lambda x, y: 1 if x != y else 0,
+    '<': lambda x, y: 1 if x < y else 0,
+    '>': lambda x, y: 1 if x > y else 0,
+    '<=': lambda x, y: 1 if x <= y else 0,
+    '>=': lambda x, y: 1 if x >= y else 0,
+    
+    }
+    def evaluate(self, scope):
+        return Number(self.dct[self.op](self.lhs.evaluate(scope).value, self.rhs.evaluate(scope).value))
+
+
+class UnaryOperation:
+    dct = {
+    '-': lambda x: -x,
+    '!': lambda x: not x
+    
+    }
+    def __init__(self, op, expr):
+        self.op = op
+        self.expr = expr
+
+    def evaluate(self, scope):
+        return Number(self.dct[self.op](self.expr.evaluate(scope).value))
+
+
+def example():
+    parent = Scope()
+    parent["foo"] = Function(('hello', 'world'),
+                             [Print(BinaryOperation(Reference('hello'),
+                                                    '+',
+                                                    Reference('world')))])
+    parent["bar"] = Number(10)
+    scope = Scope(parent)
+    assert 10 == scope["bar"].value
+    scope["bar"] = Number(20)
+    assert scope["bar"].value == 20
+    print('It should print 8: ', end=' ')
+    #print(parent["bar"].value)
+    FunctionCall(FunctionDefinition('foo', parent['foo']),
+                 [Number(5), Number(3)]).evaluate(scope)
+    print('It should print 2: ', end=' ')
+    FunctionCall(FunctionDefinition('foo', parent['foo']),
+                 [Number(5), UnaryOperation('-', Number(3))]).evaluate(scope)
+
+def my_tests():
+    a = Scope()
+    a["ee"] = Number(10)
+    print("ans 30 59 89 -29 1770 0.5084745762711864 1 0 0 30 1 1 1 0 0 1 -30 False")
+    a["foo"] = Function(('a1', 'a2',),
+                             [Print(Reference('a1')), Print(Reference('a2')),
+                              Print(BinaryOperation(Reference('a1'), '+', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '-', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '*', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '/', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '<', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '>', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '>=', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '%', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '&&', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '!=', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '||', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '==', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '>=', Reference('a2'))),
+                              Print(BinaryOperation(Reference('a1'), '<=', Reference('a2'))),
+                              Print(UnaryOperation('-', Reference('a1'))),
+                              Print(UnaryOperation('!', Reference('a1')))])
+    FunctionCall(FunctionDefinition('foo', a['foo']),
+                 [Number(30), Number(59)]).evaluate(a)
+    a["cond3"] = Function(('a1', 'a2'),
+                             [Conditional(BinaryOperation(Reference('a1'), '>', Reference('a2')), 
+                                          [Print(BinaryOperation(Reference('a1'), '-',Reference('a2')))], 
+                                          [Print(BinaryOperation(Reference('a1'), '+',Reference('a2')))])])
+    print("ans 89")
+    FunctionCall(FunctionDefinition('cond3', a['cond3']),
+                 [Number(30), Number(59)]).evaluate(a)
+    t = Scope()
+    t['foo'] = Number(2)
+    y = Scope(t)
+    u = Scope(y)
+    print(u['foo'].value)
+if __name__ == '__main__':
+    #example()
+    #my_tests()
+    pass 
