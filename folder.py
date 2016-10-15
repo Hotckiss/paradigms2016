2d42c898224a77d541b1dce139128a8dab9fdca2
Hotckiss <kirilenko_aA@mail.ru>
2016-10-15 23:10:42 +0300

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,5 +1,5 @@
-import yat.model
-import yat.printer
+import model
+import printer
 class ConstantFolder:
     def visit(self, tree):
         return tree.visit(self)
@@ -7,29 +7,29 @@ class ConstantFolder:
         return list(map(lambda x: x.visit(self), expr))
     def visitUnaryOperation(self, expr):
         tmp = expr.expr.visit(self)
-        if isinstance(tmp, yat.model.Number):
-            return yat.model.UnaryOperation(expr.op, tmp).evaluate(None)
+        if isinstance(tmp, model.Number):
+            return model.UnaryOperation(expr.op, tmp).evaluate(None)
     def visitBinaryOperation(self, expr):
         tmpl = expr.lhs.visit(self)
         tmpr = expr.rhs.visit(self)
-        if isinstance(tmpl, yat.model.Number) and isinstance(tmpr, yat.model.Number):
-            return yat.model.BinaryOperation(tmpl, expr.op, tmpr).evaluate(None)
-        elif isinstance(tmpl, yat.model.Number) and isinstance(tmpr, yat.model.Reference):
+        if isinstance(tmpl, model.Number) and isinstance(tmpr, model.Number):
+            return model.BinaryOperation(tmpl, expr.op, tmpr).evaluate(None)
+        elif isinstance(tmpl, model.Number) and isinstance(tmpr, model.Reference):
             if tmpl.value == 0:
-                return yat.model.Number(0)
-        elif isinstance(tmpl, yat.model.Reference) and isinstance(tmpr, yat.model.Number):
+                return model.Number(0)
+        elif isinstance(tmpl, model.Reference) and isinstance(tmpr, model.Number):
             if tmpr.value == 0:
-                return yat.model.Number(0)
-        elif isinstance(tmpl, yat.model.Reference) and isinstance(tmpr, yat.model.Reference) and expr.op == "-":
+                return model.Number(0)
+        elif isinstance(tmpl, model.Reference) and isinstance(tmpr, model.Reference) and expr.op == "-":
             if tmpl.name == tmpr.name:
-                return yat.model.Number(0)
-        return yat.model.BinaryOperation(tmpl, expr.op, tmpr)
+                return model.Number(0)
+        return model.BinaryOperation(tmpl, expr.op, tmpr)
             
     def visitNumber(self, extra):
         return extra
         
     def visitPrint(self, extra):
-        return yat.model.Print(extra.expr.visit(self))
+        return model.Print(extra.expr.visit(self))
         
     def visitRead(self, extra):
         return extra
@@ -38,39 +38,42 @@ class ConstantFolder:
         return extra
         
     def visitFunctionDefinition(self, extra):
-        return yat.model.FunctionDefinition(extra.name, yat.model.Function(extra.function.args, self.listev(extra.function.body)))
+        return model.FunctionDefinition(extra.name, model.Function(extra.function.args, self.listev(extra.function.body)))
     
     def visitFunctionCall(self, extra):
-        return yat.model.FunctionCall(extra.fun_expr.visit(self), self.listev(extra.args))
+        return model.FunctionCall(extra.fun_expr.visit(self), self.listev(extra.args))
         
     def visitConditional(self, extra):
-        return yat.model.Conditional(extra.condition.visit(self), self.listev(extra.if_true), self.listev(extra.if_false))
+        return model.Conditional(extra.condition.visit(self), self.listev(extra.if_true), self.listev(extra.if_false))
     
 if __name__ == '__main__':
     pass
-    """pr1 = yat.printer.PrettyPrinter()
-    a = yat.model.Scope()
-    a["ee"] = yat.model.BinaryOperation(yat.model.Number(5), "+", yat.model.BinaryOperation(yat.model.Number(6), "*", yat.model.Number(7)))
+    """pr1 = printer.PrettyPrinter()
+    a = model.Scope()
+    a["ee"] = model.BinaryOperation(model.Number(5), "+", model.BinaryOperation(model.Number(6), "*", model.Number(7)))
     pr1.visit(a['ee'])
-    a["foo"] = yat.model.Function(('a1', 'a2',),
-                             [yat.model.Print(yat.model.Reference('a1')), yat.model.Print(yat.model.Reference('a2'))])
-    a['ttt'] = yat.model.FunctionDefinition('foo', a['foo'])
+    a["foo"] = model.Function(('a1', 'a2',),
+                             [model.Print(model.Reference('a1')), model.Print(model.Reference('a2'))])
+    a['ttt'] = model.FunctionDefinition('foo', a['foo'])
     #pr1.visit(a['ttt'])
-    a['rrr'] = yat.model.FunctionCall(yat.model.Reference('foo'),
-                 [yat.model.Number(5), yat.model.Number(3)])
-    a["cond3"] = yat.model.Conditional(yat.model.Reference('a1'), [yat.model.Print(yat.model.Number(5))], None)
-    a["bazooka"] = yat.model.Function(('a1', 'a2',),
-                             [yat.model.Print(yat.model.Reference('a1')), yat.model.Print(yat.model.Reference('a2')),
-                              yat.model.Print(yat.model.BinaryOperation(yat.model.Reference('a1'), '+', yat.model.Reference('a2'))),
-                              yat.model.Print(yat.model.BinaryOperation(yat.model.Reference('a1'), '-', (yat.model.BinaryOperation(yat.model.Reference('a1'), '-', yat.model.Reference('a2'))))),
-                              yat.model.Print(yat.model.UnaryOperation('-', yat.model.Reference('a1'))),
-                              yat.model.Print(yat.model.UnaryOperation('!', yat.model.Reference('a1')))])
+    a['rrr'] = model.FunctionCall(model.Reference('foo'),
+                 [model.Number(5), model.Number(3)])
+    a["cond3"] = model.Conditional(model.Reference('a1'), [model.Print(model.Number(5))], [model.Print(model.Number(5))])
+    a["bazooka"] = model.Function(('a1', 'a2',),
+                             [model.Print(model.Reference('a1')), model.Print(model.Reference('a2')),
+                              model.Print(model.BinaryOperation(model.Reference('a1'), '+', model.Reference('a2'))),
+                              model.Print(model.BinaryOperation(model.Reference('a1'), '-', (model.BinaryOperation(model.Reference('a1'), '-', model.Reference('a2'))))),
+                              model.Print(model.UnaryOperation('-', model.Reference('a1'))),
+                              model.Print(model.UnaryOperation('!', model.Reference('a1')))])
     pr1.visit(a['ee'])
     pr1.visit(a['rrr'])    
     pr1.visit(a['cond3'])
-    pr1.visit(yat.model.FunctionDefinition('bazooka', a['bazooka']))
+    pr1.visit(model.FunctionDefinition('bazooka', a['bazooka']))
     f1 = ConstantFolder()
-    a["cond4"] = yat.model.BinaryOperation(yat.model.Number(0), '*', a['ee'])
+    a["cond4"] = model.BinaryOperation(model.Number(0), '*', a['ee'])
     pr1.visit(a['cond4'])
     a['cond4'] = f1.visit(a['cond4'])
-    pr1.visit(a['cond4'])"""
+    pr1.visit(a['cond4'])
+    number = model.Number(42)
+    pp = model.Print(number)
+    pr1.visit(pp)"""
