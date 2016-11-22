5d9fb51b533845aeeb80205a56b94ce5e4defdae
Hotckiss <kirilenko_aA@mail.ru>
2016-11-22 17:41:33 +0400

    Update wsqueue.c

--- pool2/src/wsqueue.c
+++ pool2/src/wsqueue.c
@@ -12,7 +12,7 @@ void queue_push(queue_t* queue, node_t* node) {
     node->next->prev = node;
     queue->size++;
 }
-inline int queue_size(queue_t* queue) { return queue->size; }
+int queue_size(queue_t* queue) { return queue->size; }
 node_t* queue_pop(queue_t* queue) {
     if (!queue_size(queue)) return NULL;
     node_t* node = queue->head.prev;
@@ -32,7 +32,7 @@ void wsqueue_push(wsqueue_t* queue, node_t* node) {
     pthread_cond_signal(&queue->cond);
     pthread_mutex_unlock(&queue->mutex);
 }
-inline int wsqueue_size(wsqueue_t* queue) { return queue_size(&queue->queue); }
+int wsqueue_size(wsqueue_t* queue) { return queue_size(&queue->queue); }
 void wsqueue_finit(wsqueue_t* queue) {
     pthread_mutex_destroy(&queue->mutex);
     pthread_cond_destroy(&queue->cond);
@@ -42,4 +42,3 @@ void wsqueue_notify_all(wsqueue_t* queue) {
     pthread_cond_broadcast(&queue->cond);
     pthread_mutex_unlock(&queue->mutex);
 }
-
