5a2f1da9c2f8f1b202d51a01a89602bb84674e82
Hotckiss <kirilenko_aA@mail.ru>
2016-11-28 02:00:24 +0400

    Add files via upload

---
+++ ht9/9.sql
@@ -0,0 +1,7 @@
+SELECT A.Year, B.Year, Country.Name, (A.Rate - B.Rate) / (A.Year - B.Year) FROM Country
+INNER JOIN LiteracyRate A ON Country.Code = A.CountryCode
+INNER JOIN LiteracyRate B ON Country.Code = B.CountryCode
+GROUP BY Country.Name, A.Year, B.Year
+HAVING min(B.Year) > A.Year
+ORDER BY (A.Rate - B.Rate) / (A.Year - B.Year) desc;
+
