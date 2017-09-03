91371a40901d718e20d375bbebc4e47116968884
Andrew <kirilenko_aA@mail.ru>
2017-09-03 17:10:31 +0300

    Update 9.sql

--- ht9/9.sql
+++ ht9/9.sql
@@ -2,5 +2,5 @@ SELECT A.Year, B.Year, Country.Name, (A.Rate - B.Rate) / (A.Year - B.Year) FROM 
 INNER JOIN LiteracyRate A ON Country.Code = A.CountryCode
 INNER JOIN LiteracyRate B ON Country.Code = B.CountryCode
 GROUP BY Country.Name, A.Year, B.Year
-HAVING min(B.Year) > A.Year 
+HAVING min(B.Year) > A.Year
 ORDER BY (A.Rate - B.Rate) / (A.Year - B.Year) desc;
